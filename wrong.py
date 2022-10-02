c0f3dfc86d28e37a7675754c28ea1d76ebdd01ba
terrydeng666 <terry10428@gmail.com>
2022-10-02 15:22:00 +0800

    Add minus feature
    output another line a-b

--- wrong.py
+++ wrong.py
@@ -2,5 +2,5 @@
 a = eval(input('please enter the first nubmer: '))
 b = eval(input('please enter the second number: '))
 print("The first number adding the second number is: ",a+b)
-print("The first number adding the second number is: ",a-b)
+print("The first number subtracting the second number is: ",a-b)
 
